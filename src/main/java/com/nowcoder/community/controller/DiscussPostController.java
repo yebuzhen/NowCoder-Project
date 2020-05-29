156ed01b525359be0e2e9609283b25de8bcac89d
yebuzhen <791544415@qq.com>
2020-05-29 15:55:02 +0800

    Show Comments

--- src/main/java/com/nowcoder/community/controller/DiscussPostController.java
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -1,9 +1,13 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.entity.Comment;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.CommentService;
 import com.nowcoder.community.service.DiscussPostService;
 import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Date;
+import java.util.*;
 
 @Controller
 @RequestMapping("/discuss")
-public class DiscussPostController {
+public class DiscussPostController implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
@@ -28,6 +32,9 @@ public class DiscussPostController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private CommentService commentService;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -52,7 +59,7 @@ public class DiscussPostController {
     }
 
     @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
-    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model, Page page) {
 
         //Post
         DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
@@ -61,6 +68,74 @@ public class DiscussPostController {
         //Author user
         User user = userService.findUserById(post.getUserId());
         model.addAttribute("user", user);
+
+        //Page info of comments
+        page.setLimitInOnePage(5);
+        page.setPath("/discuss/detail/" + discussPostId);
+        page.setRowsTotal(post.getCommentCount());
+
+        //Comment: the comment to the post
+        //Reply: the comment to the comment
+
+        //Comment list
+        List<Comment> commentList = commentService.findCommentByEntity(
+                ENTITY_TYPE_POST, post.getId(), page.getOffset(), page.getLimitInOnePage()
+        );
+
+        //Comment view object (VO) list
+        List<Map<String, Object>> commentVoList = new ArrayList<>();
+
+        if (commentList != null) {
+
+            for (Comment comment : commentList) {
+
+                //Comment VO
+                Map<String, Object> commentVo = new HashMap<>();
+                //Comment
+                commentVo.put("comment", comment);
+                //Author of the comment
+                commentVo.put("user", userService.findUserById(comment.getUserId()));
+
+                //Reply list
+                List<Comment> replyList = commentService.findCommentByEntity(
+                        ENTITY_TYPE_COMMENT, comment.getId(), 0, Integer.MAX_VALUE
+                );
+
+                //Reply VO list
+                List<Map<String, Object>> replyVoList = new ArrayList<>();
+
+                if (replyList != null) {
+
+                    for (Comment reply : replyList) {
+
+                        Map<String, Object> replyVo = new HashMap<>();
+                        //Reply
+                        replyVo.put("reply", reply);
+                        //Author of the reply
+                        replyVo.put("user", userService.findUserById(reply.getUserId()));
+                        //Reply target
+                        User target = reply.getTargetId() == 0 ? null : userService.findUserById(reply.getTargetId());
+                        replyVo.put("target", target);
+
+                        replyVoList.add(replyVo);
+
+                    }
+
+                }
+
+                commentVo.put("replies", replyVoList);
+
+                //The number of replies to one comment
+                int replyCount = commentService.findCommentCount(ENTITY_TYPE_COMMENT, comment.getId());
+                commentVo.put("replyCount", replyCount);
+
+                commentVoList.add(commentVo);
+
+            }
+
+        }
+
+        model.addAttribute("comments", commentVoList);
 
         return "/site/discuss-detail";
 
--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -2,11 +2,17 @@ package com.nowcoder.community.util;
 
 public interface CommunityConstant {
 
+    //Activation status
     int ACTIVATION_SUCCESS = 0;
     int ACTIVATION_FAILURE = 1;
     int ACTIVATION_DUPLICATE = 2;
 
+    //Expired time after login
     int DEFAULT_EXPIRED_SECONDS = 3600 * 12;
     int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
+
+    //Entity types
+    int ENTITY_TYPE_POST = 1;
+    int ENTITY_TYPE_COMMENT = 2;
 
 }
