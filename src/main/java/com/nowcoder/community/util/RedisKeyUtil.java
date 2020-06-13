8e10ad03aede1cefd1646e25ce788c16fd83636c
yebuzhen <791544415@qq.com>
2020-06-13 16:24:24 +0800

    Follow And Un-Follow Partial Finish

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -5,7 +5,9 @@ public class RedisKeyUtil {
 
   private static final String SPLIT = ":",
       PREFIX_ENTITY_LIKE = "like:entity",
-      PREFIX_USER_LIKE = "like:user";
+      PREFIX_USER_LIKE = "like:user",
+      PREFIX_FOLLOWER = "follower",
+      PREFIX_FOLLOWEE = "followee";
 
   // Likes for one entity
   // like:entity:entityType:entityId -> set(userId)
@@ -17,5 +19,17 @@ public class RedisKeyUtil {
   // like:user:userId -> int
   public static String getUserLikeKey(int userId) {
     return PREFIX_USER_LIKE + SPLIT + userId;
+  }
+
+  // Entities that one user follows
+  // followee:userId:entityType -> zset(entityId,now)
+  public static String getFolloweeKey(int userId, int entityType) {
+    return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+  }
+
+  // Users that one entity has
+  // follower:entityType:entityId -> zset(userId,now)
+  public static String getFollowerKey(int entityType, int entityId) {
+    return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
   }
 }
