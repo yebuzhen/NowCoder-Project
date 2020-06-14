21b19f337b6e8738b7368c73ceed9ce7dcda3dad
yebuzhen <791544415@qq.com>
2020-06-14 15:34:35 +0800

    Follower And Followee List, Partial Finish

--- src/main/java/com/nowcoder/community/service/FollowService.java
+++ src/main/java/com/nowcoder/community/service/FollowService.java
@@ -1,6 +1,14 @@
 package com.nowcoder.community.service;
 
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.RedisKeyUtil;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.core.RedisOperations;
@@ -10,9 +18,11 @@ import org.springframework.stereotype.Service;
 
 /** @author barea */
 @Service
-public class FollowService {
+public class FollowService implements CommunityConstant {
 
   @Autowired private RedisTemplate redisTemplate;
+
+  @Autowired private UserService userService;
 
   public void follow(int userId, int entityType, int entityId) {
 
@@ -75,5 +85,46 @@ public class FollowService {
 
     String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
     return redisTemplate.opsForZSet().score(followeeKey, entityId) != null;
+  }
+
+  // Query the users that one user follows
+  public List<Map<String, Object>> findUserFollowees(int userId, int offset, int limit) {
+
+    String followeeKey = RedisKeyUtil.getFolloweeKey(userId, ENTITY_TYPE_USER);
+
+    return getMapList(offset, limit, followeeKey);
+  }
+
+  // Query the followers that one user has
+  public List<Map<String, Object>> findUserFollowers(int userId, int offset, int limit) {
+
+    String followerKey = RedisKeyUtil.getFollowerKey(ENTITY_TYPE_USER, userId);
+
+    return getMapList(offset, limit, followerKey);
+  }
+
+  private List<Map<String, Object>> getMapList(int offset, int limit, String key) {
+
+    // The returned set is in order
+    Set<Integer> targetIds =
+        redisTemplate.opsForZSet().reverseRange(key, offset, offset + limit - 1);
+
+    if (targetIds == null) {
+      return null;
+    }
+
+    List<Map<String, Object>> list = new ArrayList<>();
+
+    for (Integer targetId : targetIds) {
+
+      Map<String, Object> map = new HashMap<>(2);
+      User user = userService.findUserById(targetId);
+      map.put("user", user);
+      Double score = redisTemplate.opsForZSet().score(key, targetId);
+      map.put("followTime", new Date(score.longValue()));
+      list.add(map);
+    }
+
+    return list;
   }
 }
