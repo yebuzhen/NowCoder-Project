308f6d596d7d7bfe5c2451dc2740cb790e321728
yebuzhen <791544415@qq.com>
2020-06-17 15:44:06 +0800

    Blocking Queue Demo

---
+++ src/test/java/com/nowcoder/community/BlockingQueueTests.java
@@ -0,0 +1,77 @@
+package com.nowcoder.community;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQueueTests {
+
+  public static void main(String[] args) {
+
+    BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue(10);
+    new Thread(new Producer(blockingQueue)).start();
+    new Thread(new Consumer(blockingQueue)).start();
+    new Thread(new Consumer(blockingQueue)).start();
+    new Thread(new Consumer(blockingQueue)).start();
+
+  }
+
+}
+
+class Producer implements Runnable {
+
+  private BlockingQueue<Integer> queue;
+
+  public Producer(BlockingQueue<Integer> queue) {
+    this.queue = queue;
+  }
+
+  @Override
+  public void run() {
+
+    try {
+
+      for (int i = 0; i < 100; i++) {
+
+        Thread.sleep(150);
+        queue.put(i);
+        System.out.println(Thread.currentThread().getName() + " produced one. There are " + queue.size() + " in the queue");
+
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+
+}
+
+class Consumer implements Runnable {
+
+  private BlockingQueue<Integer> queue;
+
+  public Consumer(BlockingQueue<Integer> queue) {
+    this.queue = queue;
+  }
+
+  @Override
+  public void run() {
+
+    try {
+
+      while (true) {
+
+        Thread.sleep(new Random().nextInt(1000));
+        queue.take();
+        System.out.println(Thread.currentThread().getName() + " consumed one. There are " + queue.size() + " in the queue");
+
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+
+}
